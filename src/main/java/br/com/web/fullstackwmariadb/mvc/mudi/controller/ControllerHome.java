6a1db4adeb6d91ab1e93ea753c8386a1d67e851c
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-09-06 15:54:22 +0800

    adding mysql entity and @persisenceconte at Contoller as well as 'getall'  from DB (local)

--- src/main/java/br/com/web/fullstackwmariadb/mvc/mudi/controller/ControllerHome.java
+++ src/main/java/br/com/web/fullstackwmariadb/mvc/mudi/controller/ControllerHome.java
@@ -1,7 +1,10 @@
 package br.com.web.fullstackwmariadb.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,18 +15,15 @@ import br.com.web.fullstackwmariadb.mvc.mudi.model.Pedido;
 @Controller
 public class ControllerHome {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @GetMapping("/home")
     public String home(Model model) {
 
-        Pedido pedido = new Pedido();
-        pedido.setNomeProduto("Lekebaby Nappy Bag");
-        pedido.setValorNegociado(43.98);
-        pedido.setUrlImage("https://m.media-amazon.com/images/I/61fr-HGo3DL._AC_SX679_.jpg");
-        pedido.setUrlProduto(
-                "https://www.amazon.com.au/Lekebaby-Nappy-Backpack-Diaper-Changing/dp/B08CRHZQB6/?_encoding=UTF8&pd_rd_w=3Sbmq&content-id=amzn1.sym.3a84452d-4836-4b58-a639-43fdeca1bac3&pf_rd_p=3a84452d-4836-4b58-a639-43fdeca1bac3&pf_rd_r=CGWT1THN9Z4THSNG4STC&pd_rd_wg=okiRr&pd_rd_r=6c42016c-9d34-4619-a82b-ee2be5213e5a&ref_=pd_gw_ci_mcx_mr_hp_atf_m");
-        pedido.setDescricao("Bolsa para guardar mamadeiras");
-
-        List<Pedido> pedidos = Arrays.asList(pedido);
+        TypedQuery<Pedido> query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+        List<Pedido> pedidos = query.getResultList();
+        System.out.println(pedidos);
         model.addAttribute("pedidos", pedidos);
 
         return "home";
